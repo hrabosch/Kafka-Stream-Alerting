0914c2ac28b9a109951329ac29cf522d1085e23d
Aleš Laňar <ales.lanar@gmail.com>
2024-01-30 15:42:39 +0100

    Server: Init spring boot template

---
+++ collector-server/src/main/java/org/hrabosch/collector/server/CollectorServerApplication.java
@@ -0,0 +1,13 @@
+package org.hrabosch.collector.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectorServerApplication.class, args);
+    }
+
+}
